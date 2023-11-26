9492a74648dcd9516c6574d391814447c502f25d
KOKI MATSUO <koki1021m@gmail.com>
2023-11-27 03:34:25 +0900

    add: export extensions

--- main.py
+++ main.py
@@ -2,22 +2,32 @@ from flask import Flask, request, send_file
 from PIL import Image
 import pillow_heif
 import io
+import os
 
 app = Flask(__name__)
 
 @app.route('/')
 def upload_file():
-    # HTMLコードを直接埋め込む
+    # 出力形式を選択できるフォーム
     return '''
     <!doctype html>
     <html>
     <head>
-        <title>HEIC to JPEG Converter</title>
+        <title>Image Converter</title>
     </head>
     <body>
-        <h1>HEICファイルをJPEGに変換</h1>
+        <h1>画像ファイルを変換</h1>
         <form action="/convert" method="post" enctype="multipart/form-data">
-            <input type="file" name="file">
+            <input type="file" name="file"><br><br>
+            <label for="format">出力形式を選択:</label>
+            <select name="format">
+                <option value="JPEG">JPEG</option>
+                <option value="PNG">PNG</option>
+                <option value="BMP">BMP</option>
+                <option value="GIF">GIF</option>
+                <option value="TIFF">TIFF</option>
+                <option value="WebP">WebP</option>
+            </select><br><br>
             <input type="submit" value="Upload">
         </form>
     </body>
@@ -25,26 +35,38 @@ def upload_file():
     '''
 
 @app.route('/convert', methods=['POST'])
-def convert_heic_to_jpeg():
+def convert_image():
     if 'file' not in request.files:
         return 'ファイルがありません'
     file = request.files['file']
+    output_format = request.form.get('format', 'JPEG')  # デフォルトはJPEG
+    filename, file_extension = os.path.splitext(file.filename)
     if file.filename == '':
         return 'ファイルが選択されていません'
     if file:
-        heif_file = pillow_heif.read_heif(file.stream)  # HEICファイルを読み込み
-        image = Image.frombytes(
-            "RGB", 
-            heif_file.size, 
-            heif_file.data,
-            "raw",
-            "RGB",
-            heif_file.stride,
-        )
+        if file_extension.lower() in ['.heic', '.heif']:
+            # HEIC/HEIFファイルを指定された形式に変換
+            heif_file = pillow_heif.read_heif(file.stream)
+            image = Image.frombytes(
+                "RGB", 
+                heif_file.size, 
+                heif_file.data,
+                "raw",
+                "RGB",
+                heif_file.stride,
+            )
+        else:
+            # 他の形式のファイルを選択された形式に変換
+            image = Image.open(file.stream)
+
         output = io.BytesIO()
-        image.save(output, format='JPEG')  # JPEG形式で保存
+        image.save(output, format=output_format)
         output.seek(0)
-        return send_file(output, download_name='converted_image.jpeg', as_attachment=True)
+        return send_file(
+            output,
+            as_attachment=True,
+            download_name=f'converted_image.{output_format.lower()}'
+        )
 
 if __name__ == '__main__':
     app.run(debug=True)
